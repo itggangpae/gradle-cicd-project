9b619a570112d046055fc9e56660f2de17eb1930
itggangpae <ggangpae1@gmail.com>
2024-12-23 13:44:18 +0900

    sum

--- src/main/java/com/leszko/calculator/Calculator.java
+++ src/main/java/com/leszko/calculator/Calculator.java
@@ -4,6 +4,6 @@ import org.springframework.stereotype.Service;
 @Service 
 public class Calculator { 
      public int sum(int a, int b) { 
-          return a - b;
+          return a + b;
      } 
 } 
