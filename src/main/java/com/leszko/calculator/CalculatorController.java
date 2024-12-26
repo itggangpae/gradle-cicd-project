adadcb6939313262c28c2080f8319242a179b40d
itggangpae <ggangpae1@gmail.com>
2024-12-26 09:02:58 +0900

    sonarqube

--- src/main/java/com/leszko/calculator/CalculatorController.java
+++ src/main/java/com/leszko/calculator/CalculatorController.java
@@ -1,5 +1,7 @@
 package com.leszko.calculator; 
-import org.springframework.beans.factory.annotation.Autowired; 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RequestParam; 
 import org.springframework.web.bind.annotation.RestController; 
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 class CalculatorController { 
      @Autowired
      private  Calculator calculator;
- 
+
+     private final Logger logger = LoggerFactory.getLogger(CalculatorController.class);
+
      @RequestMapping("/sum") 
      String sum(@RequestParam("a") Integer a,  
-                @RequestParam("b") Integer b) { 
+                @RequestParam("b") Integer b) {
+          logger.debug("a: {}, b: {}", a, b);
           return String.valueOf(calculator.sum(a, b)); 
      } 
 } 
