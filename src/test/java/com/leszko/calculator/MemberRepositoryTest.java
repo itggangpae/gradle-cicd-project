ba935b151442f0110fbdfbbe2019f879e7391132
itggangpae <ggangpae1@gmail.com>
2024-12-30 09:46:45 +0900

    unit test

--- src/test/java/com/leszko/calculator/MemberRepositoryTest.java
+++ src/test/java/com/leszko/calculator/MemberRepositoryTest.java
@@ -2,15 +2,10 @@ package com.leszko.calculator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@ActiveProfiles("test")
-@DataJpaTest
-@TestPropertySource(locations = "classpath:application-test.yml")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+
+@SpringBootTest
 class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
